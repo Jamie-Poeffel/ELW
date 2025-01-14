3348c33f9c2e1911b3de711fcf2ada516154494e
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2025-01-14 11:05:18 +0100

    Geändert --> app/program/update.ps1

--- app/program/update.ps1
+++ app/program/update.ps1
@@ -1,2 +1,23 @@
-git clone 'https://github.com/Jamie-Poeffel/ELW.git'
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # If not admin, restart PowerShell as admin
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"";
+    $newProcess.Verb = "runas"; # Force PowerShell to run as administrator
+    [System.Diagnostics.Process]::Start($newProcess) | Out-Null;
+    Exit;
+}
+
+$groundPath = 'C:/'
+
+git clone 'https://github.com/Jamie-Poeffel/ELW.git' $groundPath
+
+Set-Location $groundPath
+Set-Location .\ELW\app
+
+
+& install.bat
+
+Set-Location $groundPath
+ 
+Remove-Item .\ELW -Recurse -Force
 
