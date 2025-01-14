c70d9c7d8397a115dc4d3b20d772b09bfc4cda87
Jamie Poeffel <168637927+Jamie-Poeffel@users.noreply.github.com>
2025-01-14 22:57:10 +0100

    gitoFree.ps1 aktualisieren

--- app/program/gitoFree.ps1
+++ app/program/gitoFree.ps1
@@ -2,31 +2,6 @@ param (
     [switch]$AdminStarted  # Prevent infinite loops
 )
 
-if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
-    Write-Host "Git is not installed or not available in PATH." -ForegroundColor Red
-    Exit 1
-}
-
-$version_file = "C:\Windows\System32\ELW\version.json"
-$jsonUrl = 'https://raw.githubusercontent.com/Jamie-Poeffel/ELW/main/app/program/version.json'
-
-
-# Read the local JSON file and convert it to a PowerShell object
-$version = Get-Content -Path $version_file -Raw | ConvertFrom-Json
-
-$vers = $version.version.Trim()  # Trim any spaces
-
-$online_version = Invoke-RestMethod -Uri $jsonUrl -Method Get
-
-# Extract version numbers
-$onvers = $online_version.version.Trim()  # Trim any spaces
-
-# Compare versions correctly
-if ($vers -ne $onvers) {
-    Write-Host "Please update to new Version $onvers with command: elx -u / elx --update" -ForegroundColor Red
-    Exit 1
-}
-
 $errorMSG = ""
 
 # Check if running with Admin rights
