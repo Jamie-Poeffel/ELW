10beb34e01f49d7e452a0e4ebe1bfd9434f74b06
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2025-01-14 10:03:59 +0100

    Geändert --> app/program/version.ps1

--- app/program/version.ps1
+++ app/program/version.ps1
@@ -3,8 +3,10 @@ $version_file = "C:\Windows\System32\ELW\version.json"
 
 # Read the JSON file and convert it to a PowerShell object
 $version = Get-Content -Path $version_file -Raw | ConvertFrom-Json
+$jsonUrl = 'https://github.com/Jamie-Poeffel/ELW/blob/main/app/program/version.json'
 
-$online_version = Get-Content -Path 'https://github.com/Jamie-Poeffel/ELW/blob/main/app/program/version.json' -Raw | ConvertFrom-Json
+$online_version = Invoke-RestMethod -Uri $jsonUrl -Method Get
+
 
 $onvers = $online_version.version
 $vers = $version.version
@@ -12,6 +14,6 @@ $vers = $version.version
 # Access properties of the JSON object
 Write-Host "elx --- $vers"
 
-if (-not(vers -eq $onvers)) {
+if (-not($vers -eq $onvers)) {
     Write-Host "Update to new Version $onvers with command elx -u / elx --update"
 }
