f267068b104bf63d97ab0029d8bf40a11c604b27
Jamie Poeffel <168637927+Jamie-Poeffel@users.noreply.github.com>
2025-02-05 22:06:28 +0100

    Update gitoFree.ps1

--- app/program/gitoFree.ps1
+++ app/program/gitoFree.ps1
@@ -69,7 +69,7 @@ try {
 
     Start-Sleep 1
     
-    & "C:/free-gito/install.bat" "echo 'skip'"
+    & "C:/free-gito/install.bat" "git add ."
 }
 catch {
     $errorMSG += "Error while installing: $_ `n" 
